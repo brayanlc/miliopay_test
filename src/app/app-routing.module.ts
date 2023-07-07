94f87c7ab7672ef254ef301ebf141744c2e652bd
brayan <brayanlc82@gmail.com>
2023-07-07 14:55:40 -0500

    Remove placeholder content from App Component
    This commit removes large block of unnecessary placeholder content from the App Component. This content was initially added by the Angular CLI as a template for a generated application. Removal of this content will declutter the component, leaving only essential and useful code for a cleaner, easier to read file.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/auth/login/login.component').then(
+        (m) => m.LoginComponent,
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
