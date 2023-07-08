a55fb26f102d1214f349107f963a78f315e7f169
brayan <brayanlc82@gmail.com>
2023-07-08 12:09:57 -0500

    Refactor app module structure and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { AppRoutingModule } from './app/app-routing.module';
+import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom(BrowserModule, AppRoutingModule, BrowserAnimationsModule)],
+}).catch((err) => console.error(err));
