f93ec58b15f5957b400c92d419f848ca0563aeed
brayan <brayanlc82@gmail.com>
2023-07-09 22:19:29 -0500

    Add SidenavComponent and RouterOutlet to ContentLayoutComponent
    The ContentLayoutComponent has been updated to include SidenavComponent and RouterOutlet from Angular router, enabling navigation capability within the application. Styling changes for ContentLayoutComponent have also been made to support full page view with a separate side navigation bar. New changes provide a clearer layout structure and better responsiveness of the application.

--- src/app/layouts/content-layout/content-layout.component.ts
+++ src/app/layouts/content-layout/content-layout.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SidenavComponent } from '../components/sidenav/sidenav.component';
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-content-layout',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SidenavComponent, RouterOutlet],
   templateUrl: './content-layout.component.html',
-  styleUrls: ['./content-layout.component.scss']
+  styleUrls: ['./content-layout.component.scss'],
 })
-export class ContentLayoutComponent {
-
-}
+export class ContentLayoutComponent {}
