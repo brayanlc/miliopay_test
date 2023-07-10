f2ce66112998d2b7d2e546881cb85db767b8e2ae
brayan <brayanlc82@gmail.com>
2023-07-09 22:18:56 -0500

    "Add flat and outline button variants to ButtonComponent"
    This commit introduces two more component variants to the ButtonComponent: 'flat' and 'outline'. This extension enhances flexibility giving more stylistic options while using this component across different sections of the project. Both new components follow the ChangeDetectionStrategy.OnPush pattern for performance optimization.

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -11,14 +11,58 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
         border-radius: 25px;
         background-color: #0136fe;
         padding: 0.5rem 2rem;
+        width: 250px;
         color: #fff;
         font-weight: 500;
         font-size: 1.5rem;
         line-height: 1;
-        width: 250px;
       }
     `,
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ButtonComponent {}
+
+@Component({
+  selector: 'button [flat]',
+  standalone: true,
+  template: `<ng-content></ng-content>`,
+  styles: [
+    `
+      :host {
+        border: 1px #0136fe solid;
+        border-radius: 25px;
+        background-color: #0136fe;
+        padding: 0.5rem 2rem;
+        width: 250px;
+        color: #fff;
+        font-weight: 500;
+        font-size: 1.5rem;
+        line-height: 1;
+      }
+    `,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ButtonFlatComponent {}
+
+@Component({
+  selector: 'button [outline]',
+  standalone: true,
+  template: `<ng-content></ng-content>`,
+  styles: [
+    `
+      :host {
+        border: 1px #0136fe solid;
+        border-radius: 25px;
+        padding: 0.5rem 2rem;
+        color: #0136fe;
+        font-weight: 500;
+        font-size: 20px;
+        line-height: 1;
+      }
+    `,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ButtonOutlineComponent {}
