3b047237e5b9a220aa174b7a54028090978211a1
brayan <brayanlc82@gmail.com>
2023-07-10 22:35:50 -0500

    Add HttpClientModule to list of providers
    In this commit, the HttpClientModule is added to the list of providers in 'src/main.ts'. This allows the application to make HTTP requests, which is a requisite for most front-end applications to communicate with back-end services. This change helps to support any future APIs that might be used.

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppComponent } from './app/app.component';
 import { AppRoutingModule } from './app/app-routing.module';
 import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HttpClientModule } from "@angular/common/http";
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(BrowserModule, AppRoutingModule, BrowserAnimationsModule)],
+  providers: [importProvidersFrom(BrowserModule, AppRoutingModule, BrowserAnimationsModule, HttpClientModule)],
 }).catch((err) => console.error(err));
