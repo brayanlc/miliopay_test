91f6f49052a23300e04782063f446a46a7a48170
brayan <brayanlc82@gmail.com>
2023-07-10 22:35:13 -0500

    Add new export and search SVG icons
    Two new SVG icons have been added to 'src/assets/icons': 'export.svg' and 'search.svg'. These icons have been added to provide visual clues to users for the respective functionalities.

--- src/app/features/pay/components/table/table.component.ts
+++ src/app/features/pay/components/table/table.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TableTagComponent } from "../table-tag/table-tag.component";
+import { TableTagComponent } from '../table-tag/table-tag.component';
+import { Pay } from '../../pay';
 
 @Component({
   selector: 'app-table',
@@ -10,33 +11,5 @@ import { TableTagComponent } from "../table-tag/table-tag.component";
   styleUrls: ['./table.component.scss'],
 })
 export class TableComponent {
-  data = [
-    {
-      payment: 'Payment 1',
-      provider: 'Provider 1',
-      category: 'Category 1',
-      due: new Date(),
-      value: 234234,
-      responsible: 'Responsible 1',
-      status: 'Status 1',
-    },
-    {
-      payment: 'Payment 2',
-      provider: 'Provider 2',
-      category: 'Category 2',
-      due: new Date(),
-      value: 39423845,
-      responsible: 'Responsible 2',
-      status: 'Status 2',
-    },
-    {
-      payment: 'Payment 3',
-      provider: 'Provider 3',
-      category: 'Category 3',
-      due: new Date(),
-      value: 2345234,
-      responsible: 'Responsible 3',
-      status: 'Status 3',
-    },
-  ];
+  @Input() payments: Pay[] | undefined = [];
 }
