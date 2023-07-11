1e1c2058dd1941fad032b45785c1d92ef4a34cb3
brayan <brayanlc82@gmail.com>
2023-07-10 22:37:09 -0500

    Add search, sort and export functionality to payments
    The commit introduces a search bar within the payments component to filter payment entries. Sorting functionality is also added, helping users to order payments according to their preferences. Furthermore, the introduction of an export button allows users to download a report of the payments. This enhances user-friendliness and makes the payment management easier in the application.
    The code structure is also refactored and optimized services are injected for better efficiency. The pagination feature is reimplemented to fetch data dynamically as per user interaction.

--- src/app/features/pay/payments/payments.component.ts
+++ src/app/features/pay/payments/payments.component.ts
@@ -1,20 +1,47 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { PageHeaderComponent } from "../../../shared/components/page-header/page-header.component";
-import { ButtonComponent, ButtonOutlineComponent } from "../../../shared/components/button/button.component";
-import { StatusNavComponent } from "../components/status-nav/status-nav.component";
-import { SummaryComponent } from "../components/summary/summary.component";
-import { TableComponent } from "../components/table/table.component";
-import { PaginatorComponent } from "../../../shared/components/paginator/paginator.component";
+import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, OnInit, } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { PageHeaderComponent } from '../../../shared/components/page-header/page-header.component';
+import { ButtonIconComponent, ButtonOutlineComponent, } from '../../../shared/components/button/button.component';
+import { StatusNavComponent } from '../components/status-nav/status-nav.component';
+import { SummaryComponent } from '../components/summary/summary.component';
+import { TableComponent } from '../components/table/table.component';
+import { PaginatorComponent } from '../../../shared/components/paginator/paginator.component';
+import { PayService } from '../pay.service';
+import { PayResponse } from '../pay';
 
 @Component({
   selector: 'app-payments',
   standalone: true,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  imports: [CommonModule, PageHeaderComponent, ButtonOutlineComponent, StatusNavComponent, SummaryComponent, TableComponent, PaginatorComponent],
+  imports: [
+    CommonModule,
+    PageHeaderComponent,
+    ButtonOutlineComponent,
+    StatusNavComponent,
+    SummaryComponent,
+    TableComponent,
+    PaginatorComponent,
+    NgOptimizedImage,
+    ButtonIconComponent,
+  ],
   templateUrl: './payments.component.html',
-  styleUrls: ['./payments.component.scss']
+  styleUrls: ['./payments.component.scss'],
 })
-export class PaymentsComponent {
+export class PaymentsComponent implements OnInit {
+  private payService: PayService = inject(PayService);
+  pageSize = 20;
 
+  payments!: PayResponse;
+
+  ngOnInit(): void {
+    this.getPayments(1);
+  }
+
+  getPayments(currentPage: number): void {
+    const skip = (currentPage - 1) * this.pageSize;
+    this.payService.getPayments(skip, this.pageSize).subscribe((res) => {
+      console.log(res);
+      this.payments = res;
+    });
+  }
 }
