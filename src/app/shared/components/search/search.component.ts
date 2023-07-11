d6666442bfa6e218919f86e2fff2050fe0e39b5d
brayan <brayanlc82@gmail.com>
2023-07-10 22:36:46 -0500

    Add Search component and its spec file
    A new Search component has been added along with its spec file for testing. This component will be used to provide search functionality across the app. The spec file contains the initial setup for unit testing this component.

---
+++ src/app/shared/components/search/search.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-search',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      search works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class SearchComponent {
+
+}
