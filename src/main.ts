8dea19560e321f11fcc06e4e7fe89fd7cb43283c
brayan <brayanlc82@gmail.com>
2023-07-12 23:38:09 -0500

    Update package.json and main.ts with new dependencies
    Updated ng serve command in package.json to enable hosting in a network and added new dependencies - @angular/cdk and @angular/material for utilizing Material Design components in the application.
    
    Further, main.ts updated with more detailed configurations and animations support brought in provideAnimations.
    
    Additionally, package-lock.json is updated to include their dependencies and assure consistent installs.

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,20 @@ import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { AppRoutingModule } from './app/app-routing.module';
 import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { HttpClientModule } from "@angular/common/http";
+import {
+  BrowserAnimationsModule,
+  provideAnimations,
+} from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(BrowserModule, AppRoutingModule, BrowserAnimationsModule, HttpClientModule)],
+  providers: [
+    importProvidersFrom(
+      BrowserModule,
+      AppRoutingModule,
+      BrowserAnimationsModule,
+      HttpClientModule,
+    ),
+    provideAnimations(),
+  ],
 }).catch((err) => console.error(err));
